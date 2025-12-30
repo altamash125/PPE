f6558faee95f2c118456eba599e99f2d5c7703d0
Altamash Raza <altamarhraza875@gmail.com>
2025-12-30 22:57:04 +0300

    Updated leave_application.py with latest changes

--- ppecon_erp/leave_application/leave_application.py
+++ ppecon_erp/leave_application/leave_application.py
@@ -3,13 +3,12 @@ from frappe import _
 
 @frappe.whitelist()
 def submit_leave_from_mobile(**kwargs):
-    # get fields from kwargs
     employee = kwargs.get("employee")
     leave_type = kwargs.get("leave_type")
     from_date = kwargs.get("from_date")
     to_date = kwargs.get("to_date")
     incharge_replacement = kwargs.get("incharge_replacement")
-    custom_ticket = kwargs.get("custom_ticket") or "No"  # correct field name
+    custom_ticket = kwargs.get("custom_ticket") or "No"  # Correct field name!
     reason = kwargs.get("reason")
 
     # create Leave Application in draft/open state
@@ -20,7 +19,7 @@ def submit_leave_from_mobile(**kwargs):
         "from_date": from_date,
         "to_date": to_date,
         "incharge_replacement": incharge_replacement,
-        "custom_ticket": custom_ticket,  # fixed typo
+        "custom_ticket": custom_ticket,  # fixed here
         "reason": reason,
         "status": "Open"  # draft state
     })
