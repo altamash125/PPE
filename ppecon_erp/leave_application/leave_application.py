ee0ec4e50123fbff436f0aed6d9d59a22abc47fb
Altamash Raza <altamarhraza875@gmail.com>
2026-02-01 09:05:56 +0300

    fixed issues

--- ppecon_erp/leave_application/leave_application.py
+++ ppecon_erp/leave_application/leave_application.py
@@ -49,7 +49,7 @@ def submit_leave_from_mobile(**kwargs):
     doc.reload()
 
     return {
-        "message": "Leave Application Applied (Workflow)",
+        "message": "Leave Applied Successfully",
         "name": doc.name,
         "docstatus": doc.docstatus,          # 0
         "status": doc.status,                # Pending Approval
@@ -57,52 +57,3 @@ def submit_leave_from_mobile(**kwargs):
         "leave_approver": doc.leave_approver
     }
 
-# import frappe
-# from frappe import _
-
-# @frappe.whitelist()
-# def submit_leave_from_mobile(**kwargs):
-#     """
-#     Mobile API to create and submit Leave Application.
-#     """
-
-#     # Allowed ticket values
-#     allowed_values = [
-#         "Yes (On Company)",
-#         "Yes (On Employee)",
-#         "No"
-#     ]
-
-#     ticket = kwargs.get("ticket")
-#     if ticket not in allowed_values:
-#         ticket = "No"
-
-#     # Prepare Leave Application document
-#     doc = frappe.get_doc({
-#         "doctype": "Leave Application",
-#         "employee": kwargs.get("employee"),
-#         "leave_type": kwargs.get("leave_type"),
-#         "from_date": kwargs.get("from_date"),
-#         "to_date": kwargs.get("to_date"),
-#         "incharge_replacement": kwargs.get("incharge_replacement"),
-#         "ticket": ticket,
-#         "description": kwargs.get("description")  
-#     })
-
-#     # 1️Create Draft (insert the document)
-#     doc.insert(ignore_permissions=True)
-
-#     # 2️Submit via Workflow
-#     frappe.model.workflow.apply_workflow(
-#         doc,
-#         "Submit"  # Must exactly match the workflow action name
-#     )
-
-#     doc.reload()
-
-#     return {
-#         "message": "Leave Application Submitted",
-#         "name": doc.name,
-#         "workflow_state": doc.workflow_state,
-#         "description": doc.description  # Optional: return description
-#     }
